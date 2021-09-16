a41721fe9d470ccdfebea1c528f545f81a8b26d9
Mehran <pysyche@protonmail.com>
2021-09-16 10:13:20 +0430

    add new files from python course zero to mastery

---
+++ PDF/PDF.py
@@ -0,0 +1,11 @@
+import PyPDF2
+
+with open('dummy.pdf', 'rb') as file:
+    reader = PyPDF2.PdfFileReader(file)
+    # print(reader.numPages)
+    page = reader.getPage(0)
+    page.rotateClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+with open('new.pdf', 'wb') as new_file:
+    writer.write(new_file)
